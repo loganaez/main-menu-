fdf90af668b4b0f56a10cdb4a8d864eb07aa794d
loganaez <54944821+loganaez@users.noreply.github.com>
2019-09-19 10:57:01 -0500

    Add files via upload

---
+++ mobgamdev-first-project/Assets/TapDoubleTap.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapDoubleTap : MonoBehaviour
+{
+
+    float tapTimer = 0f;
+    public float doubleTapInterval = 0.2f;
+    bool tapped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            if(tapped){
+                tapTimer += Time.deltaTime;
+                if(tapTimer > doubleTapInterval){
+                    SingleTap();
+                    tapped = false;
+                }
+            }
+        }
+        if (Input.anyKeyDown)
+        {
+            if(tapped){
+                DoubleTap();
+                tapped = false;
+            } else{
+                tapped = true;
+            }
+        }
+
+
+    }
+
+    void SingleTap(){
+        Debug.Log("<color=red>Single Tap!</color>");
+        Debug.Log("timer =" + tapTimer);
+        tapTimer = 0;
+        this.GetComponent<Renderer>().material.color = Random.ColorHSV();
+    }
+
+    void DoubleTap(){
+        Debug.Log("<color=blue>double Tap!</color>");
+        Debug.Log("timer =" + tapTimer);
+        tapTimer = 0;
+        transform.localScale += Vector3.one * 0.1f;
+        if(this.transform.localScale.x > 5){
+            this.transform.localScale = Vector3.one;
+        }
+    }
+}
---
+++ mobgamdev-first-project/Assets/Testscrypt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscrypt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        Debug.Log("Edited for the git hub browers");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
